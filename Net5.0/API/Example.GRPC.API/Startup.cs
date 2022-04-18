cd0dc1b742b900d6b94ab935751956b407d887c4
thiago <thiagoautran@gmail.com>
2022-04-18 12:45:52 -0300

    .

---
+++ Net5.0/API/Example.GRPC.API/Startup.cs
@@ -0,0 +1,25 @@
+﻿using Example.GRPC.API.Controllers.V1;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Example.GRPC.API
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddGrpc();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGrpcService<StatusInvestController>();
+            });
+        }
+    }
+}
---
+++ Net5.0/Test/Example.GRPC.API.Test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Example.GRPC.API.ApplicationCore.ProtoServices.V1.StatusInvest;
+using Grpc.Net.Client;
+
+namespace Example.GRPC.API.Test
+{
+    internal class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new StatusInvest.StatusInvestClient(channel);
+
+            var actions = await client.ListActionAsync(new Empty());
+            var fiis = await client.ListFIIAsync(new Empty());
+
+            Console.WriteLine($"Action: {actions}");
+            Console.WriteLine($"FII: {fiis}");
+        }
+    }
+}
